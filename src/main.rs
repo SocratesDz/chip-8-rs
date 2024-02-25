411b4bc040ed56334c0c359de002ed186456b04c
Sócrates Díaz <sdsxd1800@gmail.com>
2024-02-25 19:59:04 -0400

    feat: wip: testing with a chip8 binary file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,20 @@
+use std::fs;
+
+use parser::parse_instruction;
+
+use crate::{instructions::Instruction, parser::ParseInstructionError};
+
 mod context;
 mod instructions;
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    let ch8_file = fs::read("assets/ibm_logo.ch8");
+    if let Ok(bytes) = ch8_file {
+        let chunks = bytes.chunks(2);
+        let instructions: Vec<Result<Instruction, ParseInstructionError>> = chunks
+            .map(|word| parse_instruction([word[0], word[1]]))
+            .collect();
+        dbg!(instructions);
+    }
 }
