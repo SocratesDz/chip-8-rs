91a7936ecf5a4c88da413cd3c143c65260e2cb28
Sócrates Díaz <sdsxd1800@gmail.com>
2025-01-19 18:59:25 -0400

    feat: complete draw instruction

--- src/context.rs
+++ src/context.rs
@@ -13,16 +13,19 @@ pub struct Context {
     pub data: Vec<u8>,
     pub keyboard_input: Option<u8>,
     pub memory_map: Vec<u8>,
+    pub graphics_buffer: Vec<Vec<u8>>,
 }
 
 impl Context {
     pub fn new(data: &[u8]) -> Context {
         let mut memory = vec![0u8; 0x200];
         memory.append(&mut Vec::from(data));
+        let mut graphics = vec![vec![0; 64]; 32];
         Context {
             data: Vec::from(data),
             memory_map: memory,
             program_counter: 0x200,
+            graphics_buffer: graphics,
             ..Default::default()
         }
     }
@@ -167,6 +170,22 @@ impl Context {
                 // - values from the Draw command should be XORed on the existing screen
                 // - if any pixel is erased, VF = 1 else 0
                 // - use modulo for the coordinates of the display
+                let x = self.registers[x as usize];
+                let y = self.registers[y as usize];
+                let start = self.i_register as usize;
+                let end = (self.i_register + n as u16) as usize;
+                let range_i = start..end;
+                let sprites = &self.memory_map[range_i];
+                let mut collision = 0;
+
+                // For each byte from sprites range
+                for (index, bit) in sprites.iter().enumerate() {
+                    let pixel_row = &mut self.graphics_buffer[(y as usize) + index];
+                    let is_collision = proccess_graphics_row(pixel_row, x, *bit);
+                    collision |= is_collision as u8;
+                }
+                self.registers[0xF] = collision;
+                self.increment_program_counter(1);
             }
             Instruction::SkipIfKeyPressed(key) => {
                 // - if keyboard_input == x { increment PC twice }
@@ -209,6 +228,10 @@ impl Context {
                 //     V[i] = memory_map[i_register + i]
                 // }
             }
+            Instruction::SetI(address) => {
+                self.i_register = address;
+                self.increment_program_counter(1);
+            }
             _ => {
                 self.increment_program_counter(1);
             }
@@ -222,10 +245,36 @@ impl Context {
     }
 }
 
+pub fn proccess_graphics_row(input: &mut Vec<u8>, x: u8, pixels: u8) -> bool {
+    let index = (x as usize / 0xF) % input.len();
+    let octet = input[index] as usize;
+    let mut collision: bool;
+
+    let result = octet ^ (pixels as usize >> x);
+    input[index] = result as u8;
+    collision = result != octet | (pixels as usize >> x);
+
+    let remainder = ((pixels >> x) << x) ^ pixels;
+
+    if remainder > 0 {
+        let carry = remainder << (8 - x);
+
+        if carry > 0 {
+            let index = (index + 1) % input.len();
+            let octet = input[index] as usize;
+            let result = octet ^ (carry as usize);
+            collision = result != octet | (carry as usize);
+            input[index] = result as u8;
+        }
+    }
+
+    collision
+}
+
 #[cfg(test)]
 mod test {
 
-    use super::Context;
+    use super::{proccess_graphics_row, Context};
 
     #[test]
     fn set_i_register_in_context() {
@@ -235,5 +284,57 @@ mod test {
         context.step();
 
         assert_eq!(context.i_register, 0x012);
+    }
+
+    #[test]
+    fn test_graphics_bitmasking() {
+        // Screen width: 64px
+        const WIDTH: usize = 8;
+        const HEIGHT: usize = 5;
+        let mut graphics_buffer = vec![
+            vec![0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
+            vec![0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
+            vec![0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
+            vec![0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
+            vec![0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
+        ];
+        let x: u8 = 0x7;
+        let y: u8 = 0x0;
+
+        let pixels = [0x81, 0x81, 0xFF, 0x81, 0x81];
+
+        for (i, pixel) in pixels.iter().enumerate() {
+            let pixel_row = &mut graphics_buffer[(y as usize) + i];
+            proccess_graphics_row(pixel_row, x, *pixel);
+        }
+        graphics_buffer.iter().for_each(|row| {
+            println!(
+                "{:08b} {:08b} {:08b} {:08b} {:08b} {:08b} {:08b} {:08b}",
+                row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]
+            );
+        });
+    }
+
+    #[test]
+    fn test_context_display_instruction() {
+        let test_data = [
+            0x00, 0xE0, 0xA2, 0x06, 0xD0, 0x05, 0x81, 0x81, 0xFF, 0x81, 0x81, 0x00,
+        ];
+        let mut context = Context::new(&test_data);
+        context.step();
+        context.step();
+
+        assert_eq!(context.i_register, 0x206);
+
+        context.step();
+
+        context.graphics_buffer.iter().for_each(|row| {
+            println!(
+                "{:08b} {:08b} {:08b} {:08b} {:08b} {:08b} {:08b} {:08b}",
+                row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7]
+            );
+        });
+
+        assert_eq!(context.registers[0xF], 0x0);
     }
 }
